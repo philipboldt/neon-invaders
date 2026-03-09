854048248f06e717febf3529bcaa821708e8656c
Philip Boldt <philip.boldt@gmx.de>
2026-03-09 09:15:10 +0100

    fix: resolve ReferenceError in WeaponManager by adding missing CONSTANTS prefix

--- src/WeaponManager.js
+++ src/WeaponManager.js
@@ -287,19 +287,19 @@ export class WeaponManager {
             if (inv.takeDamage(this.game.playerDamage)) {
               const gain = Math.floor(inv.scoreValue * 1.5);
               this.game.score += gain;
-              this.game.particles.spawnScoreText(this.game.player.x + this.game.player.w/2, this.game.player.y - UI_FEEDBACK_SCORE_Y_OFFSET, gain);
+              this.game.particles.spawnScoreText(this.game.player.x + this.game.player.w/2, this.game.player.y - CONSTANTS.UI_FEEDBACK_SCORE_Y_OFFSET, gain);
               this.game.particles.spawnExplosion(inv.x + inv.w/2, inv.y + inv.h/2, inv.color, 0, Math.PI * 2, 0);
               
               if (inv.isBoss) {
                 this.game.shake = CONSTANTS.SHAKE_BIG_EXPLOSION;
-                this.game.particles.spawnExplosion(inv.x + inv.w / 4, inv.y + inv.h / 4, inv.color, 0, Math.PI * 2, EXPLOSION_RADIUS_BOSS_HIT);
-                this.game.particles.spawnExplosion(inv.x + inv.w * 0.75, inv.y + inv.h * 0.75, inv.color, 0, Math.PI * 2, EXPLOSION_RADIUS_BOSS_HIT);
+                this.game.particles.spawnExplosion(inv.x + inv.w / 4, inv.y + inv.h / 4, inv.color, 0, Math.PI * 2, CONSTANTS.EXPLOSION_RADIUS_BOSS_HIT);
+                this.game.particles.spawnExplosion(inv.x + inv.w * 0.75, inv.y + inv.h * 0.75, inv.color, 0, Math.PI * 2, CONSTANTS.EXPLOSION_RADIUS_BOSS_HIT);
                 this.game.spawnUpgrade(inv.x + inv.w / 4, inv.y + inv.h / 2);
                 this.game.spawnUpgrade(inv.x + inv.w * 0.75, inv.y + inv.h / 2);
                 this.game.spawnUpgrade(inv.x + inv.w / 2, inv.y + inv.h / 2);
                 this.game.maxLives += CONSTANTS.STAT_POTENTIAL_GAIN;
                 this.game.maxDamage += CONSTANTS.STAT_POTENTIAL_GAIN;
-                this.game.particles.spawnScoreText(this.game.player.x + this.game.player.w / 2, this.game.player.y - UI_FEEDBACK_BOSS_POT_Y_OFFSET, "POTENTIAL INCREASED!");
+                this.game.particles.spawnScoreText(this.game.player.x + this.game.player.w / 2, this.game.player.y - CONSTANTS.UI_FEEDBACK_BOSS_POT_Y_OFFSET, "POTENTIAL INCREASED!");
               } else {
                 this.game.spawnUpgrade(inv.x, inv.y);
               }
